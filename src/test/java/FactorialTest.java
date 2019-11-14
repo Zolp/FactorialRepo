45e4f6c0cfd0acc113ebbf0eb37518a4a7a30e99
Zolp <aeellery@bsu.edu>
2019-11-14 15:08:20 -0500

    Added failing test.

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -9,4 +9,8 @@ class FactorialTest {
     {
         assertEquals(3628800, FactorialCalculator.factorial(10));
     }
+    void factorialFailOn200()
+    {
+        assertEquals(10, FactorialCalculator.factorial(200));
+    }
 }
